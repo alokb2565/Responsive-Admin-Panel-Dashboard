79b56f8d7ee88dc108dee15387a8aed1eea7a5bd
Alok Bohidar <101129329+alokb2565@users.noreply.github.com>
2022-11-28 19:07:50 +0530

    Add files via upload

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import Logo from '../../imgs/logo.png'
+import './Sidebar.css'
+
+import { SidebarData } from '../../Data/Data'
+import { UilSignOutAlt, UilBars } from '@iconscout/react-unicons';
+import {motion} from 'framer-motion'
+import { useState } from 'react';
+
+const Sidebar = () => {
+
+    const [selected, setSelected] = useState(0);
+    const [expanded, setExpanded] = useState(true);
+
+
+    const sidebarVariants = {
+        true:{
+            left: '0'
+        },
+        false:{
+            left: '-60%'
+        }
+    }
+
+  return (
+    <>
+        <div className='bars' style={expanded ? { left:'60%' }:{ left:'5%' }}
+        onClick={()=>setExpanded(!expanded)}
+        >
+        <UilBars />
+        </div>
+        <motion.div
+         className="Sidebar"
+        variants={sidebarVariants}
+        animate={window.innerWidth <= 768 ? '${expanded}' : ""}
+        
+        >
+        { /* LOGO */}
+        <div className="logo">
+            <img src={Logo} alt="" />
+            <span>
+                Sh<span>o</span>ps
+            </span>
+        </div>
+
+        {/* MENU */}
+        <div className="menu">
+                {SidebarData.map((item, index)=>{
+                    return(
+                        <div className={selected===index?'menuItem active': 'menuItem'}
+                        key={index}
+                        onClick={()=>setSelected(index)}
+                        >
+                            <item.icon/>
+                            <span>
+                                {item.heading}
+                            </span>
+                        </div>
+                    )
+                })}   
+
+                <div className="menuItem">
+                    <UilSignOutAlt/>
+                </div>
+        </div>
+    </motion.div>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/Updates/Updates.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { UpdateData } from '../../Data/Data'
+import './Updates.css'
+
+const Updates = () => {
+  return (
+   <div className="Updates">
+        {UpdateData.map((update)=>{
+            return(
+                <div className="update">
+                    <img src={update.img} alt="" />
+                    <div className="noti">
+                        <div style={{marginBottom: '0.5rem'}}>
+                            <span>{update.name}</span>
+                            <span> {update.noti}</span>
+                        </div>
+                        <span>{update.time}</span>
+                    </div>
+                        
+                </div>
+            )
+        })}
+   </div>
+  )
+}
+
+export default Updates
